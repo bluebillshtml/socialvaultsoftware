6ecce2077918ad08b66cd7227b407cec7114adc1
bluebillshtml <carplevi@outlook.com>
2025-12-06 16:22:37 -0500

    latest

---
+++ WebReference/server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3001;
+const FILE_PATH = path.join(__dirname, 'index.html');
+
+const server = http.createServer((req, res) => {
+  // Serve index.html for all routes
+  fs.readFile(FILE_PATH, (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error loading file');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
